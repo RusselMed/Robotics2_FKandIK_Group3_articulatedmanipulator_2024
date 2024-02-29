248cd355fa6441282c7ee86795c790def30cd731
KanFudz <157684612+KanFudz@users.noreply.github.com>
2024-02-29 19:06:27 +0800

    Update ARTICULATED_FKin.m

--- ARTICULATED_FKin.m
+++ ARTICULATED_FKin.m
@@ -12,17 +12,15 @@ a3 = 10;
 % if revolute joint; theta = 0, offset = 0, after offset put the value of theta
 
 H0_1 = Link ([0, a1, 0, pi/2, 0]);
-H0_1.qlim = [-pi/2 pi/2];
+H0_1.qlim = [-85 70] * pi/180;
 
 H1_2 = Link ([0, 0, a2, 0 , 0]);
-H1_2.qlim = [-pi/6 pi/2];
+H1_2.qlim = [-25 85] * pi/180;
 
 H2_3 = Link([0, 0, a3, 0, 0]);
-H2_3.qlim = [-pi/2 pi/2];
+H2_3.qlim = [-70 90] * pi/180;
 
 
 Articulated = SerialLink ([H0_1 H1_2 H2_3], 'name', 'Articulated')
 Articulated.plot ([0 0 0],'workspace',[-50 50 -50 50 0 80]) 
 Articulated.teach
-
-    
