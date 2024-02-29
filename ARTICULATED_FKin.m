f9de919deeee9f537686de87500731981bc7a073
Gelonaticss <157782959+Gelonaticss@users.noreply.github.com>
2024-02-29 13:36:05 +0800

    Update ARTICULATED_FKin.m

--- ARTICULATED_FKin.m
+++ ARTICULATED_FKin.m
@@ -11,7 +11,7 @@ a3 = 3;
 % if prismatic joint; theta = theta, d = 0, offset = 1, after offset put the value of d
 % if revolute joint; theta = 0, offset = 0, after offset put the value of theta
 
-H0_1 = Link ([0, a1, 0, pi/2, 0, 0]);
+H0_1 = Link ([0, a1, 0, pi/2, 0]);
 H0_1.qlim = [-pi/2 pi/2];
 
 H1_2 = Link ([0, 0, a2, 0 , 0, 0]);
